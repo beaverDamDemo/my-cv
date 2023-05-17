40dbe040fe47c73e57bde35d5aefde703259388f
beaverDamDemo <zdenevi@gmail.com>
2023-05-17 18:24:39 +0200

    update

--- assets/js/src/play.js
+++ assets/js/src/play.js
@@ -1,34 +1,36 @@
-$('.closeButton').on('click', function(e) {
-	$('main').removeClass('active');
-	$('.monster').addClass('active');
-	$(this).addClass('hidden')
-	$('#fontBtn').removeClass('active');
-	setTimeout(function() {
-		$('.monster').removeClass('active');
-		$('.replay-envelope').addClass('active')
-		resetMarbles()
-	}, 3300)
-})
+$(".closeButton").on("click", function (e) {
+  $("main").removeClass("active");
+  $(".monster").addClass("active");
+  $(this).addClass("hidden");
+  $("#fontBtn").removeClass("active");
+  $("#marblesWrapper").addClass("hidden");
 
-$('#fontBtn').on('click', function(e) {
-	e.preventDefault();
-	$('#fontBtn').addClass('hidden');
-	$('body').css({
-		'font-family': 'Open Sans, Arial, Helvetica',
-    'font-size': '23px'
-	})
-  $('body table').css({
-    'font-size': '0.7em'
-  })
-	$('#wrapper section').css({
-		'text-transform': 'none'
-	})
-	$('main').css({
-		'font-size' : '1.1em'
-	})
-})
+  setTimeout(function () {
+    $(".monster").removeClass("active");
+    $(".replay-envelope").addClass("active");
+    resetMarbles();
+  }, 3300);
+});
 
-$('#replay-button').on('click', function() {
-	$('.replay-envelope').removeClass('active')
-	animateMarbles()
-})
+$("#fontBtn").on("click", function (e) {
+  e.preventDefault();
+  $("#fontBtn").addClass("hidden");
+  $("body").css({
+    "font-family": "Open Sans, Arial, Helvetica",
+    "font-size": "23px",
+  });
+  $("body table").css({
+    "font-size": "0.7em",
+  });
+  $("#wrapper section").css({
+    "text-transform": "none",
+  });
+  $("main").css({
+    "font-size": "1.1em",
+  });
+});
+
+$("#replay-button").on("click", function () {
+  $(".replay-envelope").removeClass("active");
+  animateMarbles();
+});
