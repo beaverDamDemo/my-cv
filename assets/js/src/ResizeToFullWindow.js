6ddfd1f294fb81b97201cdd85c797bea87c04bce
beaverDamDemo <zdenevi@gmail.com>
2026-04-26 16:49:25 +0200

    just cleanup. BUt for some reason without touching js the marbles are not displayed corectrlyt and they may not be rotating and moving left

--- assets/js/src/ResizeToFullWindow.js
+++ assets/js/src/ResizeToFullWindow.js
@@ -3,33 +3,36 @@ var ResizeToFullWindow = (function () {
 
   var $wrapper = $('#wrapper');
 
-  function resizeWrapper () {
-    var width = window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
+  function resizeWrapper() {
+    var width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
 
-      var height = window.innerHeight
-      || document.documentElement.clientHeight
-      || document.body.clientHeight;
+    var height =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight;
 
     var ratioWidth = width / 1024;
 
     $($wrapper[0]).css({
-      'transform': 'scale(' + ratioWidth + ') translateX(-50%)',
+      transform: 'scale(' + ratioWidth + ') translateX(-50%)',
       '-webkit-transform': 'scale(' + ratioWidth + ') translateX(-50%)',
       '-moz-transform': 'scale(' + ratioWidth + ') translateX(-50%)',
       '-o-transform': 'scale(' + ratioWidth + ') translateX(-50%)',
       '-ms-transform': 'scale(' + ratioWidth + ') translateX(-50%)',
-      'left' : '50%'
+      left: '50%',
     });
-  };
+  }
   resizeWrapper();
 
   function debounce(func, wait, immediate) {
     var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
+    return function () {
+      var context = this,
+        args = arguments;
+      var later = function () {
         timeout = null;
         if (!immediate) func.apply(context, args);
       };
@@ -38,17 +41,15 @@ var ResizeToFullWindow = (function () {
       timeout = setTimeout(later, wait);
       if (callNow) func.apply(context, args);
     };
-  };
+  }
 
-  var myEfficientFn = debounce(function() {
-    console.log('debaunce');
+  var myEfficientFn = debounce(function () {
     resizeWrapper();
   }, 50);
 
   window.addEventListener('resize', myEfficientFn);
 
   return {
-    scale: resizeWrapper
-  }
-
+    scale: resizeWrapper,
+  };
 })();
